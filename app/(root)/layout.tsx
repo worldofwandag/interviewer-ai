1a9c648e15049e8927f28aea4f64e99f34620983
worldofwandag <jwandag@gmail.com>
2025-08-05 00:19:18 -0700

    implement auth and storage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,19 @@
+import { isAuthenticated } from '@/lib/actions/auth.action';
 import Image from 'next/image';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react';
 
-const RootLayout = ({ children }: {children: ReactNode}) => {
+const RootLayout = async ({ children }: {children: ReactNode}) => {
+    const isUserAuthenticated = await isAuthenticated();
+
+    if(!isUserAuthenticated) redirect('/sign-in') //now all the non logged in users, will redirect to sign in page
     return (
         <div className='root-layout'>
             <nav>
                 <Link href='/' className='flex items-center gap-2'>
                     <Image src='/logo.svg' alt='Logo' width={38} height={32} />
-                    <h2 className='text-primary-100'>PrepWise</h2>
+                    <h2 className='text-primary-100'>SimTerview</h2>
                 </Link>
             </nav>
             {children}
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -83,4 +83,32 @@ export async function signIn(params: SignInParams) {
   }
 }
 
+export async function getCurrentUser(): Promise<User | null> {
+  const cookieStore = await cookies();
 
+  const sessionCookie = cookieStore.get('session')?.value;
+
+  if(!sessionCookie) return null;
+
+  try{
+    const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+    const userRecord = await db
+    .collection('users')
+    .doc(decodedClaims.uid)
+    .get();
+
+    if(!userRecord.exists) return null;
+    return {
+      ...userRecord.data(),
+      id: userRecord.id,
+    } as User;
+  } catch(e) {
+    console.log(e)
+    return null;
+  }
+}
+
+export async function isAuthenticated() {
+  const user = await getCurrentUser();
+  return !!user; //returns existence or non existence of user into a boolean with double !!
+}
