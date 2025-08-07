fef46727fadc8b18c3ec84bd28d3e2219883463f
worldofwandag <jwandag@gmail.com>
2025-08-06 23:53:03 -0700

    implement generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/Agent';
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            <h3>Interview Generation</h3>
+            <Agent userName="You" userId="user1" type="generate" />
+        </>
+    );
+}
+
+export default Page;
