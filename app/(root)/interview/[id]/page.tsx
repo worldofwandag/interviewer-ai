51bd8100032d6d0c6fc371246f0eee6dc4d8c9a0
worldofwandag <jwandag@gmail.com>
2025-08-13 23:45:21 -0700

    show feedback

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Agent from '@/components/Agent';
 import DisplayTechIcons from '@/components/DisplayTechIcons';
 import { getCurrentUser } from '@/lib/actions/auth.action';
-import { getInterviewById } from '@/lib/actions/general.action';
+import { getFeedbackByInterviewId, getInterviewById } from '@/lib/actions/general.action';
 import { getRandomInterviewCover } from '@/lib/utils';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
@@ -12,6 +12,14 @@ const Page = async ({ params }: RouteParams) => {
     const user = await getCurrentUser();
     const interview = await getInterviewById(id);
     if(!interview) redirect('/')
+
+    const feedback = await getFeedbackByInterviewId({
+        interviewId: id,
+        userId: user?.id!
+    })
+
+    console.log(feedback)
+
     return (
         <>
             <div className='flex flex-row gap-4 justify-between'>
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,9 +4,12 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "./DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = userId && id
+     ? await getFeedbackByInterviewId({ interviewId: id, userId}) :
+    null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
     const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('MMM D, YYYY')
 
